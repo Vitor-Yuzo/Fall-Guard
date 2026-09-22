8a4356f39f239d5cb25f7ddd8b7d2e106b4c1b01
VITOR YUZO TAKEI <vitor.yuzo.profissional@gmail.com>
2026-09-22 20:11:44 -0300

    Correção

---
+++ machine_learning/monitoramento.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    results = model(frame)
+
+    annotated = results[0].plot()
+    cv2.imshow("FallGuard - YOLO Test", annotated)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
