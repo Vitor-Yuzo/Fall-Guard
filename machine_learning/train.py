b3545fdb8e9df9cfc2a651182d4c939feb4f4b58
VITOR YUZO TAKEI <vitor.yuzo.profissional@gmail.com>
2026-09-23 22:51:04 -0300

    Modelo Treinando v1
    Realizei o treinamento do modelo com 20 épocas a fim de testes

--- machine_learning/train.py
+++ machine_learning/train.py
@@ -1,12 +1,17 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
+def main():
+    model = YOLO("yolov8n.pt")
 
-model.train(
-    data="machine_learning/dataset/data.yaml",
-    epochs=20,
-    imgsz=640,
-    batch=16,
-    project="runs",
-    name="fallguard_v1"
-)
+    model.train(
+        data="machine_learning/dataset/data.yaml",
+        epochs=20,
+        imgsz=640,
+        batch=16,
+        device=0,
+        project="runs",
+        name="fallguard_v1"
+    )
+
+if __name__ == "__main__":
+    main()
